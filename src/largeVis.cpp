965d71adb21a85fd08ea7595c4844186ddaa4b74
Amos Elb <amos.elberg@me.com>
2016-05-16 04:07:33 -0400

    Breaking down gradients

--- src/largeVis.cpp
+++ src/largeVis.cpp
@@ -29,6 +29,11 @@ inline double dist(NumericVector i, NumericVector j) {
   return sum(pow(i - j, 2));
 }
 
+inline double dist(arma::vec i, arma::vec j) {
+  return sum(pow(i - j, 2));
+}
+
+
 // [[Rcpp::export]]
 void neighbors_inner( int maxIter,
                       NumericMatrix old_knns,
@@ -60,7 +65,7 @@ void neighbors_inner( int maxIter,
 
       for (int jidx = 0; jidx < oldK; jidx++) {
         j = old_knns.column(i)[jidx];
-        if (j == 0) break;
+        if (j == 0) continue;
         if (j - 1 != i && seen.insert(j - 1).second) {
           d = dist(x_i, data.row(j-1));
           heap.push(heapObject(d, j));
@@ -69,7 +74,7 @@ void neighbors_inner( int maxIter,
 
         for (int kidx = 0; kidx < oldK; kidx++) {
           k = old_knns.column(j - 1)[kidx];
-          if (k == 0) break;
+          if (k == 0) continue;
           if (k - 1 != i && seen.insert(k - 1).second) {
             d = dist(x_i, data.row(k-1));
             heap.push(heapObject(d, k));
@@ -88,110 +93,153 @@ void neighbors_inner( int maxIter,
   for (int i = 0; i < N; i++) for (int j = 0; j < K; j++) outputKnns(j,i) = nextKnns(j,i);
 };
 
-/*
- * Note changes from the paper:
- * 1. \rho is set to decline non-linearly
- * 2. The gradient applied to update y_i for the positive sample is doubled (2 * \rho)
- *
- *
- */
-
-void checkVector(NumericVector x, std::string label) {
-  if (sum(is_na(x)) + sum(is_nan(x)) + sum(is_infinite(x)) > 0)
+void checkVector(const arma::vec& x,
+                 const std::string& label) {
+  if (x.has_nan() || x.has_inf())
     Rcout << "\n Failure at " << label;
+};
+
+double objective(const arma::mat& inputs, double gamma, double alpha) {
+  double objective = log(1 / (1 + (alpha * dist(inputs.col(0), inputs.col(1)))));
+  for (int i = 2; i < 7; i++)
+    objective += gamma * log(1 - (1 / (1 + alpha * dist(inputs.col(0), inputs.col(i)))));
+  return objective;
 }
 
+void checkGrad(const arma::vec& x,
+               const arma::vec& y,
+               const arma::vec& grad,
+               bool together,
+               const string& label) {
+  double oldDist = dist(x,y);
+  double newDist = dist(x + grad, y - grad);
+  if (together && newDist > oldDist) Rcout << "\nGrad " << label << " yi " << x << " other " << y << " grad " << grad << " moved further apart.";
+  else if (! together && newDist < oldDist) Rcout << "\nGrad " << label << " yi " << x << " other " << y << " grad " << grad << "moved closer together.";
+};
+/*
+ * w * (log( 1 / (1 + (a * sqrt((b - j)^2 + (c - f)^2)^2))) + g(log(1 - (1 / (1 + (a * sqrt((b - g)^2 + (c - k)^2)^2)))) + log(1 - (1 / (1 + (a * sqrt((b - m)^2 + (c - n)^2)^2))))))
+ */
 // [[Rcpp::export]]
-void sgd(NumericMatrix coords,
+arma::mat sgd(NumericMatrix coords,
          NumericVector positiveEdges,
          NumericVector is, // vary randomly
          NumericVector js, // ordered
          NumericVector ps, // N+1 length vector of indices to start of each row j in vector is
          NumericVector ws, // w{ij}
-         int gamma,
-         int rho,
-         int minRho,
+         double gamma,
+         double rho,
+         double minRho,
          bool useWeights,
          int M,
-         int alpha,
+         double alpha,
          Function callback) {
 
-  int N = ps.length() - 1;
+  arma::vec p = as<arma::vec>(ps);
+  arma::vec i_idx = as<arma::vec>(is);
+  arma::vec j_idx = as<arma::vec>(js);
+  arma::vec pEdges = as<arma::vec>(positiveEdges);
+  arma::mat coordinates = as<arma::mat>(coords);
+  int N = p.size() - 1;
   // Calculate negative sample weights, d_{i}^0.75.
   // Stored as a vector of cumulative sums, normalized, so it can
   // be readily searched using binary searches.
   // TODO:  CREATE A FORM FOR WHEN USEWEIGHTS = TRUE
-  arma::vec negativeSampleWeights = pow(diff(ps), 0.75);
+  arma::vec negativeSampleWeights = pow(diff(p), 0.75);
   double scale = sum(negativeSampleWeights);
   negativeSampleWeights = negativeSampleWeights / scale;
-  double acc = 0;
-  for (int idx = 0; idx < N; idx++) {
-    acc+= negativeSampleWeights[idx];
-    negativeSampleWeights[idx] = acc;
-  }
+  negativeSampleWeights = cumsum(negativeSampleWeights);
+
+  // arma::vec losses = arma::vec(pEdges.size()).zero();
 
   // Iterate through the edges in the positiveEdges vector
-  #pragma omp parallel for shared(coords, rho)
-  for (int eIdx=0; eIdx < positiveEdges.length(); eIdx++) {
-    const int e_ij = positiveEdges[eIdx];
-    const int i = is[e_ij];
-    const int j = js[e_ij];
+#pragma omp parallel for
+  for (int eIdx=0; eIdx < pEdges.size(); eIdx++) {
+    const int e_ij = pEdges[eIdx];
+    int i = i_idx[e_ij];
+    int j = j_idx[e_ij];
 
-    NumericVector y_i = coords.row(i);
-    NumericVector y_j = coords.row(j);
+    const double localRho = rho - ((rho - minRho) * eIdx / (pEdges.size()));
+
+    if ((arma::randn<arma::vec>(1))[0] < 0) swap(i, j);
+
+    arma::vec y_i = coordinates.col(i);
+    arma::vec y_j = coordinates.col(j);
+
+    // arma::mat before = arma::mat(2,M + 2);
+    // arma::mat after = arma::mat(2,M + 2);
+    // before.col(0) = y_i;
+    // before.col(1) = y_j;
 
     // wij
-    const double w = 1;// (useWeights) ? ws[e_ij] : 1;
-
-    NumericVector yigrad = - alpha * (y_i-y_j)/(1 + (alpha * dist(y_i, y_j)));
-    checkVector(yigrad, "yigrad");
+    const double w = (useWeights) ? ws[e_ij] : 1;
+// should be negative below
+    arma::vec igrad = -2 * alpha * (y_i - y_j) / (1 + (alpha * dist(y_i,y_j)));
+  //  checkGrad(y_i, y_j, igrad, true, "pos");
+#pragma omp critical
+{
+    coordinates.col(j) -= w * localRho * igrad;
+    // after.col(1) = coordinates.col(j);
+}
+    // checkVector(igrad, "ijgrad");
 
     arma::vec samples = arma::randu<arma::vec>(M * 2);
     arma::vec::iterator targetIt = samples.begin();
     int sampleIdx = 1;
     int m = 0;
-   // Rcout << "\n" << i << " " << j << "\t";
+    // int ms[5];
+    // The indices of the nodes with edges to i
+    arma::vec searchVector = i_idx.subvec(p[i], p[i + 1] - 1);
     while (m < M) {
       if (sampleIdx % (M * 2) == 0) samples.randu();
       // binary search for lowest number greater than the sampled number
       const double target = targetIt[sampleIdx++ % (M * 2)];
-      arma::vec::iterator loc = std::upper_bound(negativeSampleWeights.begin(),
+      int k;
+      if (useWeights) k = target * (N - 1);
+      else {
+        arma::vec::iterator loc = std::upper_bound(negativeSampleWeights.begin(),
                                                  negativeSampleWeights.end(), target);
-      const int k = std::distance(negativeSampleWeights.begin(), loc);
-      if (k == i || k == j) continue;
-      bool tst = false;
-      for (int tstidx = ps(i); tstidx < ps(i + 1); tstidx++) if (is[tstidx] == k) {
-        tst = true;
-        break;
+        k = std::distance(negativeSampleWeights.begin(), loc);
       }
-      if (tst) continue;
-
+      if (k == i ||
+          k == j ||
+          sum(searchVector == k) > 0) continue;
+      // ms[m] = k;
       // Calculate gradient for a single negative sample
-      NumericVector y_k = coords.row(k);
-      const double dk = dist(y_i, y_k);
-      const double alphadk1 = (alpha * dk) + 1;
-      yigrad = yigrad + (
-        2 * gamma * alpha * (y_i - y_k) / (
-            pow((alpha * dk) + 1, 2) *
-               (1 - (1 / ((alpha * dk) + 1)))
-        ));
-      checkVector(yigrad, "yigradneg");
-      NumericVector kgrad = -2 * gamma * alpha * (y_i - y_k) /
-        ((1 - (1/alphadk1))*pow(alphadk1,2));
-      checkVector(kgrad, "kgrad");
-      // coords(k,_) = y_k + (kgrad * rho * w);
-      m++;
+      arma::vec y_k = coordinates.col(k);
+      const double alphadk1 = (alpha * dist(y_i, y_k)) + 1;
+      arma::vec gradk = 2 * gamma * (y_i - y_k) / (dist(y_i, y_k) * alphadk1);
+  //    checkGrad(y_i, y_k, igrad, false, "neg");
+      igrad = igrad + gradk;
+      // before.col(m + 2) = y_k;
+#pragma omp critical
+{
+      coordinates.col(k) -=  gradk * localRho * w;
+}
+// after.col(m + 2) = coordinates.col(k);
+m++;
+
+
     }
-    NumericVector yjgrad = 2 * alpha * (y_i - y_j) / (1 + (alpha * dist(y_i,y_j)));
-    checkVector(yjgrad, "jgrad");
+    // checkVector(igrad, "jgrad");
 
-    coords(i,_) = y_i + (yigrad * w * rho);
-    coords(j,_) = y_j + (yjgrad * w * rho);
-
-    #pragma omp atomic
-    rho = rho - ((rho - minRho) / (positiveEdges.length() + 1));
+#pragma omp critical
+{
+    coordinates.col(i) += igrad * w * localRho;
+}
+   // after.col(0) = coordinates.col(i);
+    // double bef = objective(before, gamma, alpha);
+    // double aft = objective(after, gamma, alpha);
+    // if (bef >= aft) Rcout << eIdx << " BAD " << bef << " to " << aft << " i " << i << " j " << j << " k " << ms << "\n";
+    //
+    // if (bef >= aft && eIdx % 10 == 0) {
+    //   for (int idx = 0; idx < M + 2; idx++) {
+    //     Rcout << before.col(idx)[0] << " " << before.col(idx)[1] << " aft " << after.col(idx)[0] << " " <<
+    //       after.col(idx)[1] << "\n";
+    //   }
+    // }
     if (eIdx > 0 && eIdx % 10000 == 0) callback(10000);
   }
+  return coordinates;
 };
 
 // Take a matrix of data and two vectors of row indices, compute the pairwise Euclidean distance,
@@ -247,54 +295,89 @@ arma::sp_mat distMatrixTowij(
   return wij;
 };
 
-// [[Rcpp::export]]
-void searchTree(int threshold, NumericVector indices,
-                NumericMatrix data, NumericMatrix output,
+void searchTree(int threshold,
+                const arma::uvec& indices,
+                const arma::mat& data,
+                arma::imat& output,
                 Function callback) {
-  if (indices.length() <= threshold) {
-    for (int i = 0; i < indices.length() - 1; i++) {
-      for (int j = i + 1; j < indices.length(); j++) {
-          output(i + j- 1, indices[i] - 1) = indices[j];
-          output(i, indices[j] - 1) = indices[i];
-      }
-    }
-    callback(indices.length());
-    return;
+  if (indices.size() < threshold) {
+  #pragma omp critical
+  {
+    int i = 0;
+    do {
+      int j = i + 1;
+      do {
+        output(j, indices[i]) = indices[j];
+        output(i, indices[j]) = indices[i];
+        j++;
+      } while (j < indices.size());
+      i++;
+    } while(i < indices.size() - 1);
+  }
+  callback(indices.size());
+  return;
   }
   // Get hyperplane
-  NumericVector selections = RcppArmadillo::sample(indices, 2, false);
-  NumericVector x1, x2, v, m;
-  x1 = data.row(selections[0] - 1);
-  x2 = data.row(selections[1] - 1);
-  v = x2 - x1;
-  m = (x1 + x2) / 2;
-  double mv = sum(m * v); // This is the hyperplane
-  NumericVector direction = NumericVector(indices.length());
-  // Calculate distances from hyperplane - partition based on which distances are > 0
-  for (int idx = 0; idx < indices.length();idx++) direction[idx] = sum(v * data.row(indices[idx] - 1)) - mv;
-  int branch = 0;
-  for (int i = 0; i < indices.length(); i++) if (direction[i] > 0) branch++;
+  arma::uvec selections = indices.elem(arma::randi<arma::uvec>(2, arma::distr_param(0, indices.size() - 1)));
+  arma::vec v =  data.col(selections[1]) - data.col(selections[0]);
+  arma::vec m = sum(data.cols(selections), 1) / 2;
+  double mv = dot(m,v); // This is the hyperplane
+  arma::vec direction = arma::vec(indices.size());
+  for (int i = 0; i < indices.size(); i++) direction[i] = sum(data.col(indices[i]) % v) - mv;
+  int branch = sum(direction > 0);
   // Don't create branches that have only 2 nodes; if the split is that lopsided, recurse and try again
-  if (branch < threshold / 3 || branch > indices.length() - (threshold / 3)) {
+  if (branch < threshold / 3 || branch > indices.size() - (threshold / 3)) {
     searchTree(threshold, indices, data, output, callback);
     return;
   }
-  // Recurse, attempt to work around segfault
-  NumericVector left = NumericVector(branch);
-  NumericVector right = NumericVector(indices.length() - branch);
-  for (int leftidx = 0, rightidx = 0;
-       leftidx + rightidx < indices.length(); ) {
-    if (direction[leftidx + rightidx] > 0) left[leftidx++] = indices[leftidx + rightidx - 1];
-    else right[rightidx++] = indices[leftidx + rightidx - 1];
+//  #pragma omp parallel sections
+  {
+//    #pragma omp section
+    {
+      searchTree(threshold, indices.elem(arma::find(direction > 0)), data, output, callback);
+    }
+//    #pragma omp section
+    {
+      searchTree(threshold, indices.elem(arma::find(direction <= 0)), data, output, callback);
+    }
   }
-#pragma omp parallel sections
-{
-#pragma omp section
-  searchTree(threshold, left, data, output, callback);
-#pragma omp section
-  searchTree(threshold, right, data, output, callback);
-}
 };
+
+// [[Rcpp::export]]
+arma::mat searchTrees(int threshold,
+                      int n_trees,
+                      NumericMatrix data, Function callback) {
+  arma::mat inputData = as<arma::mat>(data).t();
+  std::vector<std::set<int> > heap = std::vector<std::set<int> >(inputData.n_cols);
+  #pragma omp parallel for
+  for (int t = 0; t < n_trees; t++) {
+    arma::imat output = arma::imat(threshold + 1, inputData.n_cols);
+    output.fill(-1);
+    searchTree(threshold,
+               arma::regspace<arma::uvec>(0, inputData.n_cols - 1),
+               inputData,
+               output,
+               callback);
+    #pragma omp critical
+    {
+        for (int i = 0; i < inputData.n_cols; i++) for (int j = 0; j <= threshold; j++)  heap[i].insert(output(j,i));
+    }
+  }
+  int sz = 0;
+  for (int i = 0; i < heap.size(); i++) if (heap[i].size() > sz) sz = heap[i].size();
+  arma::mat output = arma::mat(sz, inputData.n_cols).zeros();
+  for (int i = 0; i < heap.size(); i++) {
+    std::set<int> neighbors = heap[i];
+    int j = 0;
+    for (std::set<int>::iterator it = neighbors.begin(); it != neighbors.end(); ++it) {
+      if (*it == 0) continue;
+      output(j, i) = *it;
+      j++;
+    }
+  }
+  return output;
+}
+
 
 // [[Rcpp::export]]
 double sigFunc(double sigma, NumericVector x_i, double perplexity) {
