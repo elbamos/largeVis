adb58647d1676bd8a89990eae743a4528a9fcfc7
elbamos <elbamos@users.noreply.github.com>
2016-06-27 13:09:04 -0400

    Update largeVis.cpp

--- src/largeVis.cpp
+++ src/largeVis.cpp
@@ -2,7 +2,7 @@
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::depends(RcppProgress)]]
-#include "largeVis.hpp"
+#include "largeVis.h"
 
 using namespace Rcpp;
 using namespace std;
