9ac6a11da65d153d45d74cd9b3bfa926bdabbdb5
Amos Elb <amos.elberg@icloud.com>
2016-09-20 19:57:55 -0400

    Fix works

--- tests/testthat/testneighbors.R
+++ tests/testthat/testneighbors.R
@@ -8,67 +8,65 @@ dat <- t(dat)
 K <- 20
 distances <- as.matrix(dist(t(dat)))
 
-convert <- function(x, edges) {
-	apply(1:(nrow(x) - 1), MARGIN = 2, FUN = function(y) {
-		edges[x[y, ], ] - edges[x[y + 1, ], ]
-	})
+check <- function(adjacency, distance) {
+	good <- 0
+	bad <- 0
+	for (i in 1:ncol(adjacency)) {
+		for (j in 2:nrow(adjacency)) {
+			if (adjacency[j, i] >= 0) {
+				if (distance[i, adjacency[j, i] + 1] < distance[i, adjacency[j - 1, i] + 1]) bad <- bad + 1
+				else good <- good + 1
+			}
+		}
+	}
+	list(good = good, bad = bad)
 }
 
 test_that("20 neighbors are sorted when max_iter = 1", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 20, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	results <- convert(neighbors, distances)
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
 K <- 40
 
 test_that("40 neighbors are sorted when max_iter = 1", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 40, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	biggers <- neighbors[-1, ]
-	smallers <- neighbors[- nrow(neighbors), ]
-	results <- biggers - smallers
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
 K <- 120
 
 test_that("120 neighbors are sorted when max_iter = 1", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 60, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	biggers <- neighbors[-1, ]
-	smallers <- neighbors[- nrow(neighbors), ]
-	results <- biggers - smallers
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
 K <- 147
 
 test_that("147 neighbors are sorted when max_iter = 1", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 70, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	biggers <- neighbors[-1, ]
-	smallers <- neighbors[- nrow(neighbors), ]
-	results <- biggers - smallers
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
+})
+
+test_that("147 neighbors are sorted when max_iter = 1 and threshodl = 140", {
+	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 140, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
 test_that("neighbors are sorted when max_iter = 0", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 40, n_trees = 10,  threads = 2, max_iter = 0, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	biggers <- neighbors[-1, ]
-	smallers <- neighbors[- nrow(neighbors), ]
-	results <- biggers - smallers
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
 test_that("neighbors are sorted when max_iter = 2", {
 	neighbors <- randomProjectionTreeSearch(dat, K = K, tree_threshold = 40, n_trees = 10,  threads = 2, max_iter = 1, verbose = FALSE)
-	good <- (neighbors != -1)[- nrow(neighbors), ]
-	biggers <- neighbors[-1, ]
-	smallers <- neighbors[- nrow(neighbors), ]
-	results <- biggers - smallers
-	expect_equal(sum(results[good] < 0), 0)
+	res <- check(neighbors, distances)
+	expect_equal(res$bad, 0)
 })
 
